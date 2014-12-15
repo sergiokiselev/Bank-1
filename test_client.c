48c878d3424db829c1c9c6dd588e95fbe8e03555
Black Beard <sergio.kiselev509@gmail.com>
2014-12-15 23:16:08 +0300

    fix client tests

--- test_client.c
+++ test_client.c
@@ -26,14 +26,14 @@ static char * all_tests() {
 	return 0;
 }
 
-int main() {
-	char *result = all_tests();
-	if (result != 0) {
-		printf("%s\n", result);
-	} else {
-		printf("ALL TESTS PASSED\n");
-	}
-	printf("Tests run: %d\n", tests_run);
-	system("pause");
-	return result != 0;
-}
+//int main() {
+//	char *result = all_tests();
+//	if (result != 0) {
+//		printf("%s\n", result);
+//	} else {
+//		printf("ALL TESTS PASSED\n");
+//	}
+//	printf("Tests run: %d\n", tests_run);
+//	system("pause");
+//	return result != 0;
+//}
